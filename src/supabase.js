ce5b1d83e913b2672509a5b9fcec06d631f2958d
nazlicanotztrk <nazlicanoztrrkk@gmail.com>
2026-01-30 17:03:30 +0300

    first commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-
-const supabaseUrl = 'https://njamhpvjuquwjvgyxpde.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qYW1ocHZqdXF1d2p2Z3l4cGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk0MjUyNTEsImV4cCI6MjA4NTAwMTI1MX0.cLkeZdCfUArJWxfrnLI_-7SkoxU_eTzFAVy9OUalwEc'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
